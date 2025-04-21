ee23c0ea30302c11fed502acb9f2824275609489
AdhamAkram <adham.akraam@gmail.com>
2025-04-21 06:36:22 +0200

    add stop stream

--- controllers/streamController.js
+++ controllers/streamController.js
@@ -19,3 +19,20 @@ exports.startStream = (req, res) => {
     return res.status(200).json({ message: "Streaming started successfully!" });
   });
 };
+exports.stopStream = (req, res) => {
+  // Execute the pkill commands to stop libcamera-vid and ffmpeg
+  exec("sudo pkill -9 libcamera-vid && sudo pkill -9 ffmpeg && sleep 2", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.status(500).json({ message: `Stream stop failed: ${error.message}` });
+    }
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return res.status(500).json({ message: `Stream stop failed: ${stderr}` });
+    }
+
+    console.log(`stdout: ${stdout}`);
+    return res.status(200).json({ message: "Streaming stopped successfully!" });
+  });
+};
