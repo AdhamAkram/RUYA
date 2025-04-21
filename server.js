5a4832acb9cb98a398ad70c962f7f0b5a716292c
AdhamAkram <adham.akraam@gmail.com>
2025-04-21 06:53:19 +0200

    port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json()); // Parse JSON body
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Use the stream routes
 app.use("/api/stream", require("./routes/streamRoutes"));  // Add this line
