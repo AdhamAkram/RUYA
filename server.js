c1578c9a3422c081e074ee93925b0db1c095476e
AdhamAkram <adham.akraam@gmail.com>
2025-04-21 14:23:42 +0200

    edit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-
+const { exec } = require("child_process");
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
