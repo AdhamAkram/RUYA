2c074b0212bb6a8eeb36a654d60e4d2dac1fc4cc
AdhamAkram <adham.akraam@gmail.com>
2025-05-13 01:37:59 +0300

    add routes

--- routes/streamRoutes.js
+++ routes/streamRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { setStreamStatus, getStreamStatus , stopStream } = require("../controllers/streamController");
+const { setStreamStatus, getStreamStatus , stopStream , getStreamUrl } = require("../controllers/streamController");
 
 router.post("/set-stream", setStreamStatus);   
 router.get("/get-stream", getStreamStatus);    
 router.post("/stop-stream", stopStream); 
+router.get("/url-stream", getStreamUrl);    
 
 module.exports = router;
