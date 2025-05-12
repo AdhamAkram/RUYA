37892accb40c31652986ab1f507aa98203f0a963
AdhamAkram <adham.akraam@gmail.com>
2025-05-13 01:49:37 +0300

    routes

--- routes/streamRoutes.js
+++ routes/streamRoutes.js
@@ -5,6 +5,6 @@ const { setStreamStatus, getStreamStatus , stopStream , getStreamUrl } = require
 router.post("/set-stream", setStreamStatus);   
 router.get("/get-stream", getStreamStatus);    
 router.post("/stop-stream", stopStream); 
-router.get("/url-stream", getStreamUrl);    
+router.get("/get-stream-url", getStreamUrl);    
 
 module.exports = router;
